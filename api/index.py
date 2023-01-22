2aad100c5e06b5921736c6441e604dae50622031
EKPyqh40 <40889428+EKPyqh40@users.noreply.github.com>
2023-01-22 23:43:04 +0100

    prutsen
    prut

---
+++ api/index.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/test')
+def test():
+    with open('../index.html', 'r') as f:
+        data = f.read()
+    return data
