83d7e9eaba93408c2513269c6f103502a54a838d
EKPyqh40 <40889428+EKPyqh40@users.noreply.github.com>
2023-01-23 23:34:59 +0100

    login screen

--- api/index.py
+++ api/index.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def home():
-    return 'Hello, World!'
+    if request.method =="POST":
+        if request.form['action'] == "create":
+            print("Create Game")
+        else:
+            game_code = request.form['game_code']
+            print(f"Join game: {game_code}")
+    with open('game.html', 'r') as f:
+        data = f.read()
+    return data
 
 @app.route('/about')
 def about():
     return 'About'
 
-@app.route('/test')
+@app.route('/game')
 def test():
-    with open('index.html', 'r') as f:
+    with open('game.html', 'r') as f:
         data = f.read()
     return data
+
